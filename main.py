5f70122066d628645815a1a6ff3c5df87adf901c
Artyom Rukavitsa <rukavitsa.a@yandex.ru>
2021-02-06 18:52:58 +0300

    новоселье на новом модуле pymysql

--- main.py
+++ main.py
@@ -39,46 +39,56 @@ def code():
         print(row)
     return render_template("events.html", events=result)
 
-#
-# @app.route('/deleteEvent/<int:id>', methods=['GET', 'POST'])
-# def deleteEvent(id):
-#     deleteQuery = f"DELETE FROM schedule WHERE idschedule=%s"
-#     cursor = db.cursor()
-#     print(id)
-#
-#     cursor.execute(deleteQuery, (id,))
-#     db.commit()
-#
-#     show_table_query = "SELECT * FROM schedule"
-#     cursor = db.cursor(True)
-#     cursor.execute(show_table_query)
-#     result = cursor.fetchall()
-#     for row in result:
-#         print(row)
-#     return redirect('/events')
-#
-#
-# @app.route('/addEvent', methods=['GET', 'POST'])
-# def addEvent():
-#     form = AddEventForm()
-#     if form.validate_on_submit():
-#         description = form.description.data
-#         start_time = form.start_time.data
-#         end_time = form.end_time.data
-#
-#
-#         cursor = db.cursor(True)
-#         cursor.execute("SELECT * FROM schedule")
-#         lastID = cursor.fetchall()[-1][0]
-#
-#         cursor.execute("INSERT INTO schedule VALUES(%s, %s, %s, %s, %s)",
-#                        (lastID + 1, lastID + 1, start_time, end_time, description, ))
-#         db.commit()
-#         return redirect('/events')
-#
-#     return render_template('addEvent.html', title='Добавление ивента', form=form)
-#
-#
+
+@app.route('/deleteEvent/<int:id>', methods=['GET', 'POST'])
+def deleteEvent(id):
+    deleteQuery = f"DELETE FROM schedule WHERE idschedule=%s"
+    cursor = connection.cursor()
+    print(id)
+
+    cursor.execute(deleteQuery, (id,))
+    connection.commit()
+
+    show_table_query = "SELECT * FROM schedule"
+    cursor = connection.cursor()
+    cursor.execute(show_table_query)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    return redirect('/events')
+
+
+@app.route('/addEvent', methods=['GET', 'POST'])
+def addEvent():
+    form = AddEventForm()
+    if form.validate_on_submit():
+        description = form.description.data
+        start_time = form.start_time.data
+        end_time = form.end_time.data
+
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM schedule")
+        lastID = cursor.fetchall()[-1]['idschedule']
+
+        cursor.execute("INSERT INTO schedule VALUES(%s, %s, %s, %s, %s)",
+                       (lastID + 1, lastID + 1, start_time, end_time, description, ))
+        connection.commit()
+        return redirect('/events')
+
+    return render_template('addEvent.html', title='Добавление ивента', form=form)
+
+
+@app.route('/eventsByDay/<int:id>', methods=['GET', 'POST'])
+def eventsByDay(id):
+    days = [None, 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+    show_table_query = "SELECT * FROM schedule"
+    cursor = connection.cursor()
+    cursor.execute(show_table_query)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+    return render_template("events.html", events=result, day=days[id])
 
 
 
